c43000b7ded9840be4ba54ce18d8375fcc1c309f
thanakrit0519 <thanakrit0519@gmail.com>
2022-10-27 21:32:00 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Clock from './clock';
+import './App.css';
+import './clock.css';
+class App extends Component {
+render() {
+return (
+<div className="App">
+<div className="clock">
+<Clock />
+</div>
+</div>
+);
+}
+}
+export default App;
